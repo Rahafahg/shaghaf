972739cab587ea83e2271ef3e7b02406ae2d08a6
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-10-17 11:00:18 +0300

    setup initial files

---
+++ lib/widgets/buttons/logout_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LogoutButton extends StatelessWidget {
+  const LogoutButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
