5eb92055af326f4874f34222f6f5764c46ee8740
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-10-17 13:33:28 +0300

    add extensions

---
+++ lib/extensions/screen_nav.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+extension ScreenNav on BuildContext {
+  push({required Widget screen, Function(dynamic)? updateInfo}) {
+    Navigator.push(this, MaterialPageRoute(builder: (context) => screen)).then((test){
+      if(updateInfo!=null) {
+        updateInfo(test);
+      }
+    });
+  }
+
+  pushReplacement({required Widget screen}) {
+    Navigator.pushReplacement(
+        this, MaterialPageRoute(builder: (context) => screen));
+  }
+
+  pushRemove({required Widget screen}) {
+    Navigator.pushAndRemoveUntil(this,
+        MaterialPageRoute(builder: (context) => screen), (predicate) => false);
+  }
+
+  pop() {
+    Navigator.pop(this);
+  }
+
+  popAndSave() {
+    Navigator.pop(this,true);
+  }
+}
