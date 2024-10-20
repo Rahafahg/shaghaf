a8cec99db4e5c67f8d589287b8333f3ef86fd354
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-10-20 18:41:20 +0300

    addin the onboarding chape

--- lib/screens/other_screens/onboarding_screen.dart
+++ lib/screens/other_screens/onboarding_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:shaghaf/constants/constants.dart';
@@ -5,6 +7,9 @@ import 'package:shaghaf/extensions/screen_nav.dart';
 import 'package:shaghaf/extensions/screen_size.dart';
 import 'package:shaghaf/screens/other_screens/select_role_screen.dart';
 import 'package:shaghaf/widgets/buttons/main_button.dart';
+import 'dart:ui' as ui;
+
+import 'package:shaghaf/widgets/chapes/onboarding_chape.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -16,51 +21,74 @@ class OnboardingScreen extends StatelessWidget {
       ["Learn", "and improve your skills in many fields"],
       ["Follow", "your passion wherever it might be"]
     ];
-    final key = GlobalKey<IntroductionScreenState>(); // to switch onboarding screen
+    final key =
+        GlobalKey<IntroductionScreenState>(); // to switch onboarding screen
     return Scaffold(
-      body: IntroductionScreen(
-        key: key,
-        dotsContainerDecorator: const BoxDecoration(color: Color(0xffFFFCEB)),
-        dotsDecorator: const DotsDecorator(activeColor: Constants.mainOrange, activeSize: Size(15,15)),
-        overrideNext: const SizedBox(height: 30),
-        overrideDone: const SizedBox(height: 30),
-        globalBackgroundColor: Colors.transparent,
-        controlsMargin: EdgeInsets.zero,
-        // scrollPhysics: NeverScrollableScrollPhysics(), // uncomment me to disable scroll
-        pages: List.generate(3, (index){
-          return PageViewModel(
-            decoration: const PageDecoration(fullScreen: true, contentMargin: EdgeInsets.zero, bodyFlex: 0),
-            useScrollView: false,
-            title: "",
-            image: Image.asset("assets/images/onboarding_pic${index+1}.png", width: context.getWidth(),height: context.getHeight(),fit: BoxFit.cover,),
-            bodyWidget: Stack(
-              children: [
-                Image.asset('assets/images/onboarding_container.png'),
-                Positioned(
-                  bottom: 50,
-                  left: 41,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(phrases[index][0],style: Theme.of(context).textTheme.titleLarge,),
-                      const SizedBox(height: 8),
-                      Text(phrases[index][1],style: Theme.of(context).textTheme.titleSmall,),
-                      const SizedBox(height: 50),
-                      MainButton(
-                        onPressed: () => index!=2 ? key.currentState?.next() : context.push(screen: const SelectRoleScreen()),
-                        text: index!=2 ? "Next" : "Continue",
-                        width: 290,
-                        height: 45,
-                      )
-                    ],
-                  ),
+        body: IntroductionScreen(
+            key: key,
+            dotsContainerDecorator:
+                const BoxDecoration(color: Color(0xffFFFCEB)),
+            dotsDecorator: const DotsDecorator(
+                activeColor: Constants.mainOrange, activeSize: Size(15, 15)),
+            overrideNext: const SizedBox(height: 30),
+            overrideDone: const SizedBox(height: 30),
+            globalBackgroundColor: Colors.transparent,
+            controlsMargin: EdgeInsets.zero,
+            // scrollPhysics: NeverScrollableScrollPhysics(), // uncomment me to disable scroll
+            pages: List.generate(3, (index) {
+              return PageViewModel(
+                decoration: const PageDecoration(
+                    fullScreen: true,
+                    contentMargin: EdgeInsets.zero,
+                    bodyFlex: 0),
+                useScrollView: false,
+                title: "",
+                image: Image.asset(
+                  "assets/images/onboarding_pic${index + 1}.png",
+                  width: context.getWidth(),
+                  height: context.getHeight(),
+                  fit: BoxFit.cover,
                 ),
-              ],
-            ),
-          );
-        })
-      )
-    );
+                bodyWidget: Stack(
+                  children: [
+                    //Add this CustomPaint widget to the Widget Tree
+                    CustomPaint(
+                      size: Size(
+                          context.getWidth(), context.getHeight(divideBy: 2.3)),
+                      painter: RPSCustomPainter(),
+                    ),
+                    Positioned(
+                      bottom: 50,
+                      left: 41,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            phrases[index][0],
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            phrases[index][1],
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                          const SizedBox(height: 50),
+                          MainButton(
+                            onPressed: () => index != 2
+                                ? key.currentState?.next()
+                                : context.push(
+                                    screen: const SelectRoleScreen()),
+                            text: index != 2 ? "Next" : "Continue",
+                            width: 290,
+                            height: 45,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            })));
   }
 }
