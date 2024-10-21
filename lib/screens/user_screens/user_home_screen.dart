5e51400bd9e0436a6f3f93610cb01b99786e2f16
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-10-21 20:23:38 +0300

    svaing

---
+++ lib/screens/user_screens/user_home_screen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:shaghaf/constants/constants.dart';
+import 'package:shaghaf/extensions/screen_nav.dart';
+import 'package:shaghaf/extensions/screen_size.dart';
+import 'package:shaghaf/screens/auth_screens/bloc/auth_bloc.dart';
+import 'package:shaghaf/screens/user_screens/user_notification_screen.dart';
+import 'package:shaghaf/widgets/cards/workshope_card.dart';
+
+class UserHomeScreen extends StatelessWidget {
+  const UserHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Constants.backgroundColor,
+      appBar: PreferredSize(
+        preferredSize:
+            Size(context.getWidth(), context.getHeight(divideBy: 13)),
+        child: AppBar(
+          backgroundColor: Constants.backgroundColor,
+          title: Padding(
+            padding: const EdgeInsets.only(bottom: 8, top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Hello Najwa",
+                            style: TextStyle(
+                                fontSize: 16, color: Constants.lightOrange)),
+                        Text(
+                          "Welcome to Shaghaf",
+                          style: TextStyle(
+                              fontSize: 16, color: Constants.mainOrange),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () =>
+                      context.push(screen: const UserNotificationScreen()),
+                  icon: const HugeIcon(
+                    icon: HugeIcons.strokeRoundedNotification01,
+                    color: Constants.lightGreen,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: SizedBox(
+                height: 45,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Constants.lightGreen,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 16, bottom: 12),
+                  child: Text(
+                    "Workshop of the week",
+                    style: TextStyle(fontSize: 18, color: Constants.textColor),
+                  ),
+                )
+              ],
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/pasta_workshop.png",
+                  fit: BoxFit.cover,
+                  // width: context.getWidth(),
+                  height: 200,
+                ),
+                 Center(
+                  child: Text(
+                    "Pasta Workshop",
+                style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+                const Positioned(
+                  bottom: 10,
+                  right: 18,
+                  child: Row(
+                    children: [
+                      Text(
+                        "4.6",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                
+                workshopCard(
+                  title: "Pasat Workshope",
+                  subCatigory: "Cooking",
+                  date: "Jan 30 - Feb 2, 2024",
+                  rate: "4.2",
+                ),
+                workshopCard(
+                  title: "Pasat Workshope",
+                  subCatigory: "Cooking",
+                  date: "Jan 30 - Feb 2, 2024",
+                  rate: "4.2",
+                ),
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
