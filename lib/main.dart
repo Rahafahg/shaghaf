fc31cebdb335353ffb4a3c88ecae186c5af53cb5
Rahafahg <irahaf.88@gmail.com>
2024-10-21 14:27:20 +0300

    buttom nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shaghaf/data_layer/auth_layer.dart';
+import 'package:shaghaf/screens/navigation_screen/navigation_screen.dart';
 import 'package:shaghaf/screens/other_screens/onboarding_screen.dart';
 import 'package:shaghaf/screens/other_screens/select_categories_screen.dart';
-import 'package:shaghaf/screens/user_screens/user_home_screen.dart';
 import 'package:shaghaf/services/setup.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:shaghaf/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setup();
   runApp(DevicePreview(
-      enabled: true,
-      builder: (context) => const MainApp(),
-    ));
+    enabled: true,
+    builder: (context) => const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -22,11 +23,14 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: GetIt.I.get<AuthLayer>().user != null ?
-        GetIt.I.get<AuthLayer>().didChooseFav ? const UserHomeScreen()
-        : const SelectCategoriesScreen()
-      : const OnboardingScreen()
-    );
+        theme: myappTheme,
+        debugShowCheckedModeBanner: false,
+        home: GetIt.I.get<AuthLayer>().user != null
+            ? GetIt.I.get<AuthLayer>().didChooseFav
+                ? const NavigationScreen()
+                : const SelectCategoriesScreen()
+            : const OnboardingScreen());
   }
 }
+
+  
