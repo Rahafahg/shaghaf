e9a882a00078f9b4a2442468aa7c989adeff344a
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-10-23 22:37:12 +0300

    modify the card style

--- lib/widgets/cards/workshope_card.dart
+++ lib/widgets/cards/workshope_card.dart
@@ -10,175 +10,236 @@ class WorkshopCard extends StatelessWidget {
   final WorkshopGroupModel workshop;
   final String shape;
   final Function()? onTap;
-  const WorkshopCard({super.key,required this.workshop, this.shape='square', this.onTap});
+  const WorkshopCard(
+      {super.key, required this.workshop, this.shape = 'square', this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onTap,
-      child: shape=='square' ? Container(
-        width: context.getWidth(divideBy: 2.3),
-        height: context.getHeight(divideBy: 3.5),
-        decoration: BoxDecoration(
-          color: Colors.green,
-          borderRadius: BorderRadius.circular(10.0),
-          image: DecorationImage(
-            image: workshop.image.isNotEmpty ? Image.network(workshop.image).image : const AssetImage("assets/images/pasta_workshop.png"),
-            fit: BoxFit.cover,
-          ),
-          boxShadow: [BoxShadow(color: Colors.grey.shade300,blurRadius: 4,offset: const Offset(4, 8))],
-        ),
-        child: Column(
-          children: [
-            const Spacer(),
-            Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0),topRight: Radius.circular(10.0)),
-                color: Constants.cardColor,
-              ),
-              width: context.getWidth(),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
+        onTap: onTap,
+        child: shape == 'square'
+            ? Container(
+                width: context.getWidth(divideBy: 2.3),
+                height: context.getHeight(divideBy: 3.5),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: workshop.image.isNotEmpty
+                        ? Image.network(workshop.image).image
+                        : const AssetImage("assets/images/pasta_workshop.png"),
+                    fit: BoxFit.cover,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade300,
+                        blurRadius: 4,
+                        offset: const Offset(4, 8))
+                  ],
+                ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      workshop.title,
-                      style: const TextStyle(fontFamily: "Poppins",fontSize: 12,fontWeight: FontWeight.w200,),
-                    ),
-                    Text(
-                      GetIt.I.get<DataLayer>().categories.firstWhere((category)=>category.categoryId==workshop.categoryId).categoryName,
-                      style: const TextStyle(fontSize: 10, fontFamily: "Poppins", fontWeight: FontWeight.w300),
-                    ),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        const Icon(Icons.calendar_today,size: 16, color: Colors.grey),
-                        const SizedBox(width: 4),
-                        Text(
-                          workshop.workshops.isEmpty ? 'handle me later' : workshop.workshops.first.date,
-                          style: const TextStyle(fontFamily: "Poppins", fontSize: 8, fontWeight: FontWeight.w300),
+                    const Spacer(),
+                    Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10.0),
+                            topRight: Radius.circular(10.0)),
+                        color: Constants.cardColor,
+                      ),
+                      width: context.getWidth(),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              workshop.title,
+                              style: TextStyle(
+                                  fontFamily: "Poppins",
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w300,
+                                  color: Constants.textColor),
+                            ),
+                            Text(
+                              GetIt.I
+                                  .get<DataLayer>()
+                                  .categories
+                                  .firstWhere((category) =>
+                                      category.categoryId ==
+                                      workshop.categoryId)
+                                  .categoryName,
+                              style: const TextStyle(
+                                  fontSize: 10,
+                                  fontFamily: "Poppins",
+                                  fontWeight: FontWeight.w300),
+                            ),
+                            const SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Icon(Icons.calendar_today,
+                                    size: 16, color: Constants.mainOrange),
+                                const SizedBox(width: 4),
+                                Text(
+                                  workshop.workshops.isEmpty
+                                      ? 'handle me later'
+                                      : workshop.workshops.first.date,
+                                  style: const TextStyle(
+                                      fontFamily: "Poppins",
+                                      fontSize: 8,
+                                      fontWeight: FontWeight.w300),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 5),
+                            Row(
+                              children: [
+                                const Icon(Icons.star,
+                                    size: 16, color: Colors.orange),
+                                const SizedBox(width: 4),
+                                Text(workshop.rating.toString(),
+                                    style: const TextStyle(
+                                        fontFamily: "Poppins", fontSize: 8)),
+                              ],
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        const Icon(Icons.star, size: 16, color: Colors.orange),
-                        const SizedBox(width: 4),
-                        Text(workshop.rating.toString(),style: const TextStyle(fontFamily: "Poppins", fontSize: 8)),
-                      ],
-                    ),
+                      ),
+                    )
                   ],
                 ),
-              ),
-            )
-          ],
-        ),
-      )
-      : Container(
-        alignment: Alignment.center,
-        width: context.getWidth(divideBy: 1.1),
-        height: context.getHeight(divideBy: 6.2),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Constants.cardColor,
-          border: Border.all(color: Constants.appGreyColor),
-          boxShadow: [BoxShadow(color: Colors.grey.shade300,blurRadius: 4,offset: const Offset(4, 8))],
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(topLeft: Radius.circular(10),bottomLeft: Radius.circular(10)),
-              child: SizedBox(
-                width: context.getWidth(divideBy: 3.5),
-                child: workshop.image.isNotEmpty ? Image.network(workshop.image, fit: BoxFit.cover,) : Image.asset("assets/images/pasta_workshop.png",),
               )
-            ),
-            const SizedBox(width: 10),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: context.getWidth(divideBy: 1.85),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        width: context.getWidth(divideBy: 2.5),
-                        child: Text(
-                          workshop.title,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          const Icon(Icons.star, size: 16, color: Colors.orange),
-                          const SizedBox(width: 4),
-                          Text(workshop.rating.toString(),
-                              style: Theme.of(context).textTheme.bodySmall),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Text(
-                  GetIt.I.get<DataLayer>().categories.firstWhere((category)=>category.categoryId == workshop.categoryId).categoryName,
-                  style: const TextStyle(fontSize: 14, color: Constants.mainOrange),
-                ),
-                Row(
-                  children: [
-                    CircleAvatar(
-                        radius: 12,
-                        child: workshop.workshops.first.instructorImage.isNotEmpty ? Image.network(workshop.workshops.first.instructorImage) : Image.asset(
-                            "assets/images/default_organizer_image.png")),
-                    const SizedBox(width: 4),
-                    Text(
-                      workshop.workshops.first.instructorName,
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
+            : Container(
+                alignment: Alignment.center,
+                width: context.getWidth(divideBy: 1.1),
+                height: context.getHeight(divideBy: 6.2),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Constants.cardColor,
+                  border: Border.all(color: Constants.appGreyColor),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade300,
+                        blurRadius: 4,
+                        offset: const Offset(4, 8))
                   ],
                 ),
-                SizedBox(
-                  width: context.getWidth(divideBy: 1.85),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.calendar_today,
-                              size: 16, color: Colors.grey),
-                          const SizedBox(width: 4),
-                          Text(
-                            workshop.workshops.first.date,
-                            style: Theme.of(context).textTheme.bodySmall,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10)),
+                        child: SizedBox(
+                          width: context.getWidth(divideBy: 3.5),
+                          child: workshop.image.isNotEmpty
+                              ? Image.network(
+                                  workshop.image,
+                                  fit: BoxFit.cover,
+                                )
+                              : Image.asset(
+                                  "assets/images/pasta_workshop.png",
+                                ),
+                        )),
+                    const SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: context.getWidth(divideBy: 1.85),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                width: context.getWidth(divideBy: 2.5),
+                                child: Text(
+                                  workshop.title,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(Icons.star,
+                                      size: 16, color: Colors.orange),
+                                  const SizedBox(width: 4),
+                                  Text(workshop.rating.toString(),
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall),
+                                ],
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Icon(HugeIcons.strokeRoundedUser,
-                              size: 16, color: Constants.lightGreen),
-                          const SizedBox(width: 4),
-                          Text("${workshop.workshops.first.price} SR",
-                              style: Theme.of(context).textTheme.bodySmall),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        Text(
+                          GetIt.I
+                              .get<DataLayer>()
+                              .categories
+                              .firstWhere((category) =>
+                                  category.categoryId == workshop.categoryId)
+                              .categoryName,
+                          style: const TextStyle(
+                              fontSize: 14, color: Constants.mainOrange),
+                        ),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                                radius: 12,
+                                child: workshop.workshops.first.instructorImage
+                                        .isNotEmpty
+                                    ? Image.network(workshop
+                                        .workshops.first.instructorImage)
+                                    : Image.asset(
+                                        "assets/images/default_organizer_image.png")),
+                            const SizedBox(width: 4),
+                            Text(
+                              workshop.workshops.first.instructorName,
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: context.getWidth(divideBy: 1.85),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  const Icon(Icons.calendar_today,
+                                      size: 16, color: Colors.grey),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                    workshop.workshops.first.date,
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(HugeIcons.strokeRoundedUser,
+                                      size: 16, color: Constants.lightGreen),
+                                  const SizedBox(width: 4),
+                                  Text("${workshop.workshops.first.price} SR",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-              ],
-            )
-          ],
-        ),
-      )
-    );
+              ));
   }
 }
