ff28c584cb49f965b262484f551d8fec70a0c552
Rahafahg <irahaf.88@gmail.com>
2024-11-05 11:58:09 +0300

    handel online in ticket

--- lib/widgets/cards/ticket_card.dart
+++ lib/widgets/cards/ticket_card.dart
@@ -1,10 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hugeicons/hugeicons.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:shaghaf/constants/constants.dart';
 import 'package:shaghaf/extensions/screen_size.dart';
 import 'package:shaghaf/models/booking_model.dart';
 import 'package:shaghaf/models/workshop_group_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TicketCard extends StatelessWidget {
   const TicketCard({
@@ -24,7 +27,7 @@ class TicketCard extends StatelessWidget {
       decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.surfaceTint,
           borderRadius: BorderRadius.circular(20)),
-      height: context.getHeight(divideBy: 1.5),
+      padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
       width: context.getWidth(),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
@@ -48,42 +51,47 @@ class TicketCard extends StatelessWidget {
                 )),
               ),
             ),
-            const SizedBox(
-              height: 15,
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                const Icon(Icons.numbers,
+                    size: 16, color: Constants.mainOrange),
+                const SizedBox(width: 5),
+                Text("Tickets num".tr()),
+                Text(" ${booking.numberOfTickets}")
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                const Icon(Icons.calendar_today,
+                    size: 16, color: Constants.mainOrange),
+                const SizedBox(width: 5),
+                Text("Booked at".tr()),
+                Text(" ${booking.bookingDate.toString().split(".").first}")
+              ],
             ),
             Row(
               children: [
                 const Icon(Icons.numbers,
                     size: 16, color: Constants.mainOrange),
-                const SizedBox(
-                  width: 5,
-                ),
-                Row(
-                  children: [
-                    Text("Tickets num".tr()),
-                    Text(" ${booking.numberOfTickets}")
-                  ],
-                )
+                const SizedBox(width: 5),
+                Text("Booking number".tr(context: context)),
+                const SizedBox(height: 5),
+                Text(booking.qrCode, style: const TextStyle(fontSize: 16)),
+                IconButton(
+                    onPressed: () async {
+                      await Clipboard.setData(
+                          ClipboardData(text: booking.qrCode));
+                    },
+                    icon: const Icon(HugeIcons.strokeRoundedCopy01))
               ],
             ),
-            Row(
-              children: [
-                const Icon(Icons.calendar_today,
-                    size: 16, color: Constants.mainOrange),
-                const SizedBox(
-                  width: 5,
-                ),
-                Row(
-                  children: [
-                    Text("Booked at".tr()),
-                    Text(" ${booking.bookingDate.toString().split(".").first}")
-                  ],
-                )
-              ],
-            ),
+            const SizedBox(height: 15),
             const Divider(),
-            Text(workshopGroup.title,
-                style: const TextStyle(fontSize: 20, fontFamily: "Poppins")),
+            const SizedBox(height: 15),
+            Text(workshopGroup.title, style: const TextStyle(fontSize: 20)),
+            const SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -113,9 +121,38 @@ class TicketCard extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(
-              height: 10,
-            ),
+            workshop.isOnline == true
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 15),
+                      Row(
+                        children: [
+                          const Icon(
+                            HugeIcons.strokeRoundedVideo02,
+                            size: 16,
+                          ),
+                          const SizedBox(width: 5),
+                          Text("Meeting Url".tr(context: context),
+                              style: const TextStyle(fontSize: 16)),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      InkWell(
+                        onTap: () async {
+                          if (workshop.meetingUrl != null) {
+                            final Uri url = Uri.parse(workshop.meetingUrl!);
+                            if (!await launchUrl(url)) {
+                              throw Exception('Could not launch $url');
+                            }
+                          }
+                        },
+                        child: Text(workshop.meetingUrl ?? "",
+                            style: const TextStyle(fontSize: 16)),
+                      ),
+                    ],
+                  )
+                : const SizedBox.shrink()
           ],
         ),
       ),
